aa986d9c27979c6a273e71fa158d051654417b3e
Marcos Santos <marcosalmeida1977@gmail.com>
2020-04-17 02:54:04 -0700

    update react logo inside the readme file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from "react";
+/**
+ * Project: Go Stack ReactJS Challenge Concept
+ * Author: Marcos Santos
+ * Date: April, 15, 2020.
+ * Description: This project aims to show the concepts
+ * and techniques presented and learned in the interface
+ * ReactJS training.
+ * File: app.js
+ * https://github.com/Rocketseat/bootcamp-gostack-desafios/tree/master/desafio-conceitos-nodejs
+ */
 
+import React, { useEffect, useState } from "react";
+import api from './services/api';
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: 'Marcos Santos GitHub',
+      url: 'https://github.com/maasantos10/',
+      techs:['Node.js', 'ReactJS']
+    })
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+    setRepositories(repositories.filter(
+      repository => repository.id != id
+    ))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
+        
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
